9d0138d80bc65d873e6fb560914c1f71b03db9f1
TANVIR0222 <tanvirislam3912@gmail.com>
2026-07-21 04:39:07 +0600

    feat: add homeCarousel and homeCategory data structures with product listings

---
+++ src/utils/ui-data.ts
@@ -0,0 +1,106 @@
+export const homeCarousel = [
+  {
+    id: 1,
+    // titile: "Summer Sale - Up to 50% Off!",
+    // des: "Limited time offer on selected items",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 2,
+    // titile: "Summer Sale - Up to 50% Off!",
+    // des: "Limited time offer on selected items",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 3,
+    // titile: "Summer Sale - Up to 50% Off!",
+    // des: "Limited time offer on selected items",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 4,
+    // titile: "Summer Sale - Up to 50% Off!",
+    // des: "Limited time offer on selected items",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+]
+export const homeCategory = [
+  {
+    id: 1,
+    titile: "Electronics",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 2,
+    titile: "99 Plus",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 3,
+    titile: "Gadgets",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 4,
+    titile: "Jewellery",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 5,
+    titile: "Fashion",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 6,
+    titile: "Daily Bazzar",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 7,
+    titile: "Pharmacy",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+  {
+    id: 9,
+    titile: "Stationery",
+    image: require("@/assets/app-icon/app-logo.png"),
+  },
+]
+
+export const PRODUCTS = [
+  {
+    id: "1",
+    name: "Potato Regular (± 50 Gm)",
+    price: 26,
+    image: "https://amiraheshop.com/images/product/202607170221361.jpeg",
+    favorite: false,
+  },
+  {
+    id: "2",
+    name: "Fresh Tomato (500 Gm)",
+    price: 48,
+    image: "https://images.unsplash.com/photo-1546094096-0df4bcaaa337?w=500",
+    favorite: false,
+  },
+  {
+    id: "3",
+    name: "Onion Premium (1 Kg)",
+    price: 72,
+    image: "https://amiraheshop.com/images/product/202607170959351.jpg",
+    favorite: false,
+  },
+  {
+    id: "4",
+    name: "Carrot Fresh (500 Gm)",
+    price: 38,
+    image: "https://amiraheshop.com/images/product/202607170221361.jpeg",
+    favorite: false,
+  },
+  {
+    id: "5",
+    name: "Broccoli Organic",
+    price: 95,
+    image: "https://amiraheshop.com/images/product/202607171216481.jpeg",
+    favorite: false,
+  },
+]
