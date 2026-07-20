af3bfc493545ebc4296c93209b6c9d3f56000439
TANVIR0222 <tanvirislam3912@gmail.com>
2026-07-20 23:06:51 +0600

    feat: add tailwind configuration for responsive design and color themes

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        sm: "380px",
+        md: "420px",
+        lg: "680px",
+        tablet: "1024px",
+      },
+      colors: {
+        primaryBg: "#F8F8F6",
+        primaryText: "#41410B",
+        primaryBody: "#898FA2",
+        primaryBtn: "#FF8C00",
+      },
+    },
+  },
+}
