72d8c09a7e99af4d84fb245ac0576be7be3e4e13
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-07-29 20:43:36 +0600

    fix: enhance APP_ENV determination to include APP_VARIANT

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,9 @@ import type { ConfigContext, ExpoConfig } from "expo/config"
 
 type AppEnv = "development" | "staging" | "production"
 
-const APP_ENV = (process.env.APP_ENV ?? "development") as AppEnv
+const APP_ENV = (process.env.APP_ENV ??
+  process.env.APP_VARIANT ??
+  "development") as AppEnv
 
 const IS_DEV = APP_ENV === "development"
 const IS_PROD = APP_ENV === "production"
