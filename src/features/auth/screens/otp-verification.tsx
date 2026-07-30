a7504fea614f2d4a309fa3730654af682d0fb7ab
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-07-30 21:24:33 +0600

    feat(auth): add OTP verification screen with input handling and timer

---
+++ src/features/auth/screens/otp-verification.tsx
@@ -0,0 +1,133 @@
+import { Card } from "@/components/ui/card"
+import { HeroPanel } from "@/components/ui/hero-panel"
+import KeyboardAvoidingWrapper from "@/components/ui/KeyboardAvoidingWrapper"
+import MainButton from "@/components/ui/MainButton"
+import { Screen } from "@/components/ui/screen"
+import tw from "@/lib/tailwind"
+import { router } from "expo-router"
+import React from "react"
+import { Text, TouchableOpacity, View } from "react-native"
+import { OtpInput } from "react-native-otp-entry"
+
+export default function OtpVerificationScreen() {
+  const [otpVerify, setOtpVerify] = React.useState<string>("")
+  const [seconds, setSeconds] = React.useState(30)
+
+  // const { email, from } = useLocalSearchParams<{
+  //   email: string
+  //   from?: string
+  // }>()
+
+  React.useEffect(() => {
+    if (seconds <= 0) return
+
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev - 1)
+    }, 10000)
+
+    return () => clearInterval(timer)
+  }, [seconds])
+
+  const handleRestOTP = async () => {
+    // try {
+    //   const res = await resend_otp_email({
+    //     email: email,
+    //   }).unwrap()
+    //   if (res?.status) {
+    //     Alert.alert(
+    //       "Success",
+    //       "OTP has been sent successfully, check your email"
+    //     )
+    //     setOtpVerify("")
+    //   }
+    // } catch (error: any) {
+    //   console.log(error?.message)
+    // }
+  }
+
+  const handleOtpVerify = async () => {
+    // if (otpVerify.length === 6) {
+    //   try {
+    //     const res = await verify_otp_verify({ otp: otpVerify }).unwrap()
+    //     if (res?.status) {
+    //       // Save token
+    //       storage.set("token", res?.data?.token)
+    //       if (from === "reset-password") {
+    //         router.push("/(auth)/change-password")
+    //       } else {
+    //         router.push("/(drawer)/(tabs)")
+    //       }
+    //       // Navigate to destination
+    //       // router.push(destination);
+    //     }
+    //   } catch (error: any) {
+    //     alert(error?.message)
+    //   }
+    // } else {
+    //   Alert.alert("OTP Error", "Please enter a valid 6-digit OTP code.")
+    // }
+  }
+  return (
+    <KeyboardAvoidingWrapper>
+      <Screen scroll={false} contentStyle={{ justifyContent: "center" }}>
+        <HeroPanel
+          eyebrow="Change Password"
+          title="Update Your Password"
+          body="Create a strong password that you'll use the next time you sign in."
+        />
+        <Card>
+          <OtpInput
+            focusColor="#58C1F0"
+            placeholder="000000"
+            autoFocus={false}
+            numberOfDigits={6}
+            type="numeric"
+            onFilled={(text: string) => setOtpVerify(text)}
+            textInputProps={{
+              accessibilityLabel: "One-Time Password",
+            }}
+            textProps={{
+              accessibilityRole: "text",
+              accessibilityLabel: "OTP digit",
+              allowFontScaling: false,
+            }}
+            theme={{
+              pinCodeTextStyle: {
+                color: "#757575",
+                fontSize: 20,
+                fontFamily: "InterBold",
+              },
+              pinCodeContainerStyle: {
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 100,
+                width: 50,
+                height: 50,
+              },
+            }}
+          />
+          <View style={tw`flex-row justify-between items-center `}>
+            <View style={tw`flex-row items-center`}>
+              <Text style={tw`text-text_gray text-xs ml-2`}>
+                Resend ({seconds}s)
+              </Text>
+            </View>
+
+            <TouchableOpacity onPress={() => handleRestOTP()}>
+              <Text style={tw`text-text_gray text-xs underline`}>
+                Send code again
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <MainButton
+            title={"Continue"}
+            onPress={() => router.push("/(auth)/change-password")}
+            isLoading={false}
+            textStyle={tw`text-white`}
+            showSignUpLink={false}
+          />
+        </Card>
+      </Screen>
+    </KeyboardAvoidingWrapper>
+  )
+}
