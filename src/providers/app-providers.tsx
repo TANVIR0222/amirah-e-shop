b3839b01216b7074030a75fdb8ceb3a4825abb3e
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-08-02 21:51:14 +0600

    fix(app-providers): wrap AppProviders component with Redux Provider

--- src/providers/app-providers.tsx
+++ src/providers/app-providers.tsx
@@ -1,13 +1,17 @@
+import store from "@/api/store/store"
 import { SessionProvider } from "@/features/auth/auth-session"
 import { WorkspaceProvider } from "@/features/workspace/workspace-store"
 import { AppThemeProvider } from "@/theme/theme-provider"
+import { Provider } from "react-redux"
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
-    <AppThemeProvider>
-      <SessionProvider>
-        <WorkspaceProvider>{children}</WorkspaceProvider>
-      </SessionProvider>
-    </AppThemeProvider>
+    <Provider store={store}>
+      <AppThemeProvider>
+        <SessionProvider>
+          <WorkspaceProvider>{children}</WorkspaceProvider>
+        </SessionProvider>
+      </AppThemeProvider>
+    </Provider>
   )
 }
