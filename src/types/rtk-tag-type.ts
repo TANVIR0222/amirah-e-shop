cebd8fe7027806c90059f29754e605108cbc3ad9
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-08-04 00:58:09 +0600

    feat(tags): add category tag to tagTypes and update tagTypesList

--- src/types/rtk-tag-type.ts
+++ src/types/rtk-tag-type.ts
@@ -2,6 +2,8 @@
 export enum tagTypes {
   profile = "profile",
   auth = "auth",
+  category = "category",
+  // product = "product",
 }
 
-export const tagTypesList = [tagTypes.profile, tagTypes.auth]
+export const tagTypesList = [tagTypes.profile, tagTypes.auth, tagTypes.category]
