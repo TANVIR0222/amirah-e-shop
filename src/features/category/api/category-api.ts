a4c107fbb1af57785a4547e180d533bd20398a5d
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-08-15 02:02:57 +0600

    feat: add dynamic order summary modal, category-specific drawer

--- src/features/category/api/category-api.ts
+++ src/features/category/api/category-api.ts
@@ -3,28 +3,49 @@ import { CategoryPayload } from "@/types/api-paginated-payload"
 import { PaginatedResponse } from "@/types/api-paginated-response"
 import { ApiResponse } from "@/types/api-response"
 import { tagTypes } from "@/types/rtk-tag-type"
+import { CategoryResponse } from "@/features/home/types/home-api-type"
 import { Standard } from "../type/category-type"
 
 export const categoryApi = api.injectEndpoints({
   overrideExisting: true,
   endpoints: (builder) => ({
-    getProducts: builder.query<
+    getCategoryProducts: builder.query<
       ApiResponse<PaginatedResponse<Standard>>,
       CategoryPayload
     >({
-      query: ({ page = 1, per_page = 1, id, search }) => ({
-        url: `/products`,
+      query: ({ page = 1, per_page = 1, id, search }) => {
+        return {
+          url: `/products`,
+          method: "GET",
+          params: {
+            page,
+            per_page,
+            category_id: id,
+            search,
+          },
+        }
+      },
+      providesTags: [tagTypes.shop],
+    }),
+    getCategory: builder.query<
+      ApiResponse<PaginatedResponse<CategoryResponse>>,
+      CategoryPayload
+    >({
+      query: ({ page = 1, per_page = 1000 }) => ({
+        url: `/categories`,
         method: "GET",
         params: {
           page,
           per_page,
-          category_id: id,
-          search,
         },
       }),
-      providesTags: [tagTypes.shop],
+      providesTags: [tagTypes.category],
     }),
   }),
 })
 
-export const { useGetProductsQuery, useLazyGetProductsQuery } = categoryApi
+export const {
+  useGetCategoryProductsQuery,
+  useLazyGetCategoryProductsQuery,
+  useGetCategoryQuery,
+} = categoryApi
