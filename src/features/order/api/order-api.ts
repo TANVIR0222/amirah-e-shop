70f34ff79e1bab8d02526dea4e16980d1b424f7c
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-08-19 23:22:49 +0600

    feat: implement order cancellation and refund request functionality in order screen

--- src/features/order/api/order-api.ts
+++ src/features/order/api/order-api.ts
@@ -1,13 +1,18 @@
 import { api } from "@/api/base-api/api"
 import { tagTypes } from "@/types/rtk-tag-type"
 import { CategoryPayload } from "@/types/api-paginated-payload"
-import { ApiResult } from "../type/order-type"
+import {
+  ApiResult,
+  ProductReturnRespose,
+  ReturnResponse,
+  TrackOrderInfoAndLiveLocationRespose,
+} from "../type/order-type"
 
 export const shopApi = api.injectEndpoints({
   overrideExisting: true,
   endpoints: (builder) => ({
     getAllInfoOrder: builder.query<ApiResult, CategoryPayload>({
-      query: ({ page = 1, per_page = 1, status }) => ({
+      query: ({ page = 1, per_page = 10, status }) => ({
         url: "/orders",
         method: "GET",
         params: {
@@ -19,7 +24,58 @@ export const shopApi = api.injectEndpoints({
 
       providesTags: [tagTypes.orders],
     }),
+    userOrderCanche: builder.mutation<ApiResult, string>({
+      query: (id) => ({
+        url: `/orders/${id}/cancel`,
+        method: "POST",
+      }),
+      invalidatesTags: [tagTypes.orders],
+    }),
+    trackOrderinfoAndLiveLocation: builder.query<
+      TrackOrderInfoAndLiveLocationRespose,
+      string
+    >({
+      query: (id) => ({
+        url: `/orders/${id}/track-live`,
+      }),
+      providesTags: [tagTypes.orders],
+    }),
+    orderReturnRequest: builder.mutation<
+      ProductReturnRespose,
+      { id: string; data: FormData }
+    >({
+      query: ({ id, data }) => ({
+        url: `/orders/${id}/return`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: [tagTypes.orders],
+    }),
+    returnRequestListByUser: builder.query<
+      import("@/types/api-response").ApiResponse<
+        import("@/types/api-paginated-response").PaginatedResponse<ReturnResponse>
+      >,
+      CategoryPayload
+    >({
+      query: ({ page = 1, per_page = 10 }) => ({
+        url: `/returns`,
+        method: "GET",
+        params: {
+          page,
+          per_page,
+        },
+      }),
+      providesTags: [tagTypes.orders],
+    }),
   }),
 })
 
-export const { useGetAllInfoOrderQuery, useLazyGetAllInfoOrderQuery } = shopApi
+export const {
+  useLazyReturnRequestListByUserQuery,
+  useReturnRequestListByUserQuery,
+  useOrderReturnRequestMutation,
+  useTrackOrderinfoAndLiveLocationQuery,
+  useGetAllInfoOrderQuery,
+  useLazyGetAllInfoOrderQuery,
+  useUserOrderCancheMutation,
+} = shopApi
