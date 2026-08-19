97c45380cb05e4e35a2a5d31892fd62aa7660e7b
TANVIR0222 <TANVIR0222@users.noreply.github.com>
2026-08-19 18:13:06 +0600

    feat: update profile UI to include change password option and add CRUD endpoints for user addresses

--- src/features/profile/api/profile-api.ts
+++ src/features/profile/api/profile-api.ts
@@ -1,7 +1,11 @@
 import { api } from "@/api/base-api/api"
 import { ApiResponse } from "@/types/api-response"
 import { tagTypes } from "@/types/rtk-tag-type"
-import { ProfileResponse, ProfileUpdateResponse } from "../types/profile-type"
+import {
+  AddAddressResponse,
+  ProfileResponse,
+  ProfileUpdateResponse,
+} from "../types/profile-type"
 
 export const authenticationApi = api.injectEndpoints({
   overrideExisting: true,
@@ -9,6 +13,12 @@ export const authenticationApi = api.injectEndpoints({
     userProfile: builder.query<ApiResponse<ProfileResponse>, void>({
       query: () => ({
         url: "/user/profile",
+      }),
+      providesTags: [tagTypes.profile],
+    }),
+    userAddress: builder.query<ApiResponse<ProfileResponse>, void>({
+      query: () => ({
+        url: "/user/addresses",
       }),
       providesTags: [tagTypes.profile],
     }),
@@ -26,7 +36,44 @@ export const authenticationApi = api.injectEndpoints({
       }),
       invalidatesTags: [tagTypes.profile],
     }),
+    addUserAddress: builder.mutation<ApiResponse<AddAddressResponse>, FormData>(
+      {
+        query: (payload) => ({
+          url: "/user/addresses",
+          method: "POST",
+          body: payload,
+        }),
+        invalidatesTags: [tagTypes.profile],
+      }
+    ),
+    deleteUserAddress: builder.mutation<
+      ApiResponse<AddAddressResponse>,
+      { id: string }
+    >({
+      query: ({ id }) => ({
+        url: `/user/addresses/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.profile],
+    }),
+    updateUserAddress: builder.mutation<
+      ApiResponse<AddAddressResponse>,
+      { id: string; payload: any }
+    >({
+      query: ({ id, payload }) => ({
+        url: `/user/addresses/${id}`,
+        method: "PUT",
+        body: payload,
+      }),
+      invalidatesTags: [tagTypes.profile],
+    }),
   }),
 })
-export const { useUserProfileQuery, useUpdateProfileMutation } =
-  authenticationApi
+export const {
+  useUpdateUserAddressMutation,
+  useDeleteUserAddressMutation,
+  useUserProfileQuery,
+  useUpdateProfileMutation,
+  useUserAddressQuery,
+  useAddUserAddressMutation,
+} = authenticationApi
